305786d99b6dfb284c51450e02c9ceb77b0e048a
bando9 <bandomega1@gmail.com>
2025-10-07 10:06:35 +0700

    feat: add function calculate age, show contacts; add icon; add birthdate property

--- index.js
+++ index.js
@@ -1,32 +1,65 @@
-console.log("Bearmentor - bandomega.com");
+console.log("Direcbook");
 
-// ID, full name, phone, email, address
+// id, full name, phone, email, address
 const contactsData = [
   {
-    ID: 1,
+    id: 1,
     fullName: "Warren Edward Buffett",
     phone: "081454423694",
     email: "warrenbuffett@bhhsservice.com",
     address: "Omaha, Nebraska, USA",
+    birthdate: new Date(1930, 7, 30),
   },
   {
-    ID: 2,
+    id: 2,
     fullName: "Larry Elison",
     phone: "081006330738",
     email: "lefoundation@outlook.com",
     address: "The Bronx, New York City, USA",
+    birthdate: new Date(1944, 7, 17),
   },
   {
-    ID: 3,
+    id: 3,
     fullName: "Charles Thomas Munger",
     phone: "081837956124",
     email: "charliemunger@bhhsservice.com",
     address: "Omaha, Nebraska, USA",
+    birthdate: new Date(1924, 0, 1),
   },
 ];
 
-console.log(contactsData);
+// console.log(contactsData);
 
-for (let i = 0; i < contactsData.length; i++) {
-  console.log(contactsData[i]);
+function calculateAge(yearBirthdate) {
+  const currentYear = 2025;
+
+  if (typeof yearBirthdate == "object") {
+    const getYearDate = yearBirthdate.getFullYear();
+    const age = currentYear - getYearDate;
+    return age;
+  } else {
+    const age = currentYear - yearBirthdate;
+    return age;
+  }
 }
+
+calculateAge(contactsData[1].birthdate);
+calculateAge(1999); // 26
+
+// CRUD
+
+// function addContact(id, fullName, phone, email, address){
+
+// }
+
+function showContacts(contacts) {
+  for (let i = 0; i < contacts.length; i++) {
+    const age = calculateAge(contactsData[i].birthdate);
+
+    console.log(
+      `👤 ${contacts[i].fullName} | 📞 ${contacts[i].phone} | 📧 ${contacts[i].email} | 📍 ${contacts[i].address} | 🎂 ${age}`
+    );
+  }
+}
+
+showContacts(contactsData);
